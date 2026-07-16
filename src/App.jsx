1e1c22cd1d27ab440d8436e773a21df44cbfb20e
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-07-17 00:35:00 +0500

    code set

--- src/App.jsx
+++ src/App.jsx
@@ -15,90 +15,59 @@
 
 
 
-import React, { useState } from 'react'
-const App = () => {
-  // let counter = 0
-  const [counter , setCounter] = useState(0)
- 
- const Changecounter = (feature) => {
-    console.log('counter added' , feature);
-if (feature==="add") {
- setCounter(counter + 1)
-  return
-}
-counter > 0 ? setCounter(counter - 1) : null
-
-
-
- }
- return(
-  <>
-     <h1>{counter }Counter App {counter}</h1>
-       <button onClick={()=> changeCounter('add')}>Add {counter} </button>
-      <button onClick={()=> changeCounter('less')}>Less {counter} </button>
-  
-  </>
-)
-}
-
-
-
-
-export default App
-
-
-
-
-
 // import React, { useState } from 'react'
-
 // const App = () => {
 //   // let counter = 0
 //   const [counter , setCounter] = useState(0)
-
-//   const changeCounter = (feature)=>{
+ 
+//  const Changecounter = (feature) => {
 //     console.log('counter added' , feature);
-//     if(feature === 'add'){
-//       setCounter(counter + 1)
-//       return 
-//     }
+// if (feature==="add") {
+//  setCounter(counter + 1)
+//   return
+// }
+// counter > 0 ? setCounter(counter - 1) : null
 
-//     counter > 0 ?
-//     setCounter(counter - 1) : null
-//   }
-//   return (
-//     <>
-//       <h1>{counter }Counter App {counter}</h1>
-//       <button onClick={()=> changeCounter('add')}>Add {counter} </button>
-//       <button onClick={()=> changeCounter('less')}>Less {counter} </button>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo neque eveniet {counter} id molestiae sunt iste eos {counter}, {counter} placeat {counter} doloribus ut earum nemo dicta consectetur obcaecati! Voluptates, {counter}.</p>
-//     </>
-//   )
+
+
+//  }
+//  return(
+//   <>
+//      <h1>{counter }Counter App {counter}</h1>
+//        <button onClick={()=> Changecounter('add')}>Add {counter} </button>
+//       <button onClick={()=> Changecounter('less')}>Less {counter} </button>
+  
+//   </>
+// )
 // }
 
+
+
+
 // export default App
-// import React, { useState } from "react";
 
-// const App = () => {
-//   const [counter, setCounter] = useState(0);
 
-//   const Changecounter = (feature) => {
-//     if (feature === "add") {
-//       setCounter(counter + 1);
-//       return;
-//     }
 
-//     counter > 0 ? setCounter(counter - 1) : null;
-//   };
 
-//   return (
-//     <>
-//       <h1>Welcome to Counting App {counter}</h1>
 
-//       <button onClick={() => Changecounter("add")}>Add</button>
-//       <button onClick={() => Changecounter("less")}>Less</button>
-//     </>
-//   );
-// };
 
-// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
