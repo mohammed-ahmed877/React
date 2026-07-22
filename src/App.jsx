30f8f15708929882564e63cab6cf23be09a54249
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-07-22 13:25:38 +0500

    app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 
-// import React, { useState } from 'react'
-// const App = () => {
-//   // let counter = 0
-//   const [counter , setCounter] = useState(0)
+import React, { useState } from 'react'
+const App = () => {
+  // let counter = 0
+  const [counter , setCounter] = useState(0)
  
-//  const Changecounter = (feature) => {
-//     console.log('counter added' , feature);
-// if (feature==="add") {
-//  setCounter(counter + 1)
-//   return
-// }
-// counter > 0 ? setCounter(counter - 1) : null
+ const Changecounter = (feature) => {
+    console.log('counter added' , feature);
+if (feature==="add") {
+ setCounter(counter + 1)
+  return
+}
+counter > 0 ? setCounter(counter - 1) : null
 
 
 
-//  }
-//  return(
-//   <>
-//      <h1>{counter }Counter App {counter}</h1>
-//        <button onClick={()=> Changecounter('add')}>Add {counter} </button>
-//       <button onClick={()=> Changecounter('less')}>Less {counter} </button>
-  
-//   </>
-// )
-// }
+ }
+return (
+  <div className="container">
+    <h1>Counter App</h1>
+
+    <div className="counter">{counter}</div>
+
+    <div className="buttons">
+      <button
+        className="add"
+        onClick={() => Changecounter("add")}
+      >
+        Add
+      </button>
+
+      <button
+        className="less"
+        onClick={() => Changecounter("less")}
+      >
+        Less
+      </button>
+    </div>
+  </div>
+);
+}
 
 
-
-
-// export default App
+export default App
 
 
 
@@ -48,74 +61,68 @@
 
 
 
+// import { useState } from "react"
 
-import { useState } from "react"
 
-const app =()=>{
-    const[input, setInput]=useState('');
-   const [todo , settodo]= useState([]);
-const addtodo = (event)=>{
-    event.preventDefault()
-    console.log(input);
-    settodo([...todo,input])
-    setInput('')//bad ma input ko empty kara ga
+// const App =()=>{
+//     const[input, setInput]=useState('');
+//    const [todo , setTodo]= useState([]);
+// const addtodo = (event)=>{
+//     event.preventDefault()
+//     console.log(input);
+//     setTodo([...todo,input])
+//     setInput('')//bad ma input ko empty kara ga
 
     
-}
-const updatetodo= (feature , index)=>{
-    console.log(feature , index);
+// }
+// const updatetodo= (feature , index)=>{
+//     console.log(feature , index);
      
-    if (feature === "delete") {
-        todo.splice(index , 1);
-        settodo([...todo])
-        return
+//     if (feature === "delete") {
+//         todo.splice(index , 1);
+//         setTodo([...todo])
+//         return
         
-    }
-    const updateval = prompt("enter udated val" , todo [index])
-    settodo([...todo])
+//     }
+//     const updateval = prompt("enter udated val" , todo [index])
+//     todo.splice(index , 1 , updateval)
+//     setTodo([...todo])
        
        
+// }
        
-       return (
-       <>
-        <h1>hello from todo app</h1>
-        <form onSubmit={addtodo}>
-        <input type="text" placeholder='enter todo' value={input} onChange={(e)=> setInput (e.target.value)}  />
-        <button type='submit'>add todo</button>
+//        return (
+//       <div className="container">
+//         <h1>hello from todo app</h1>
+//         <form onSubmit={addtodo}>
+//         <input type="text" placeholder='enter todo' value={input} onChange={(e)=> setInput (e.target.value)}  />
+//         <button type='submit'>add todo</button>
         
-        </form>
+//         </form>
         
-       <ol>
-        {todo.map ((item , index) =>{
-            return <li key = {index} > {item}
-            <button onClick={()=> updatetodo("delete",index)}>delete</button>
-            <button onClick={()=> updatetodo("edit",index)}>edit</button>
-                
-                
-                
-                
-                 </li>
-        }
-    
-    
-    
-    )}
-        
-        </ol> 
-        </>
-        )
-        }
+//           <ol>
+//       {todo.map((item, index) => (
+//         <li key={index}>
+//           <span>{item}</span>
 
-}
+//           <button onClick={() => updatetodo("delete", index)}>
+//             Delete
+//           </button>
+
+//           <button onClick={() => updatetodo("edit", index)}>
+//             Edit
+//           </button>
+//         </li>
+//       ))}
+//     </ol>
+//   </div>
+// );
+
+// }
 
 
 
-
-
-
-
-
-export default app
+// export default App
 
 
 
